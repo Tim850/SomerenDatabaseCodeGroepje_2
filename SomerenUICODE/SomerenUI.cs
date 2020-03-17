5cb7e64d4466c25bdf0d388491bdeb53b4561399
Tim850 <54938017+Tim850@users.noreply.github.com>
2020-03-17 10:39:20 +0100

    Add files via upload

--- SomerenUICODE/SomerenUI.cs
+++ SomerenUICODE/SomerenUI.cs
@@ -1,4 +1,4 @@
-using SomerenLogic;
+﻿using SomerenLogic;
 using SomerenModel;
 using System;
 using System.Collections.Generic;
@@ -201,10 +201,11 @@ namespace SomerenUI
                 listViewStockDrinks.FullRowSelect = true;
 
                 // Aanmaken van kollomen
-                listViewStockDrinks.Columns.Add("Name of drink");
+                listViewStockDrinks.Columns.Add("Drank Id");
+                listViewStockDrinks.Columns.Add("Drank Naam");
                 listViewStockDrinks.Columns.Add("Stock");
-                listViewStockDrinks.Columns.Add("Voucher price");
-
+                listViewStockDrinks.Columns.Add("Voucher prijs");
+                listViewStockDrinks.Columns.Add("");
 
 
             }
@@ -221,7 +222,8 @@ namespace SomerenUI
                 // show stock
                 pnl_CheckOut.Show();
 
-
+                // combobox studenten vullen
+                DisplayStudents();
             }
         }
 
@@ -273,6 +275,19 @@ namespace SomerenUI
         private void CheckOutToolStripMenuItem_Click(object sender, EventArgs e)
         {
             showPanel("CheckOut");
+        }
+
+        private void DisplayStudents()
+        {
+            List<Student> students = studService.GetStudents();
+
+            cmb_Students.Items.Clear();
+            foreach (Student student in students)
+            {
+                cmb_Students.Items.Add(student);
+            }
+
+            cmb_Students.SelectedIndex = 0;
         }
     }
 }
