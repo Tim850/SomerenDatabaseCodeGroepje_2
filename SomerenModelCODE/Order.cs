c872bb0369ef9404ee9158405064df17b1815d38
InvaderCipher <55230649+InvaderCipher@users.noreply.github.com>
2020-03-20 21:14:53 +0100

    Add files via upload

---
+++ SomerenModelCODE/Order.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomerenModel
+{
+    public class Order
+    {
+        public int OrderNumber { get; set; }
+        public Int16 DrinkID { get; set; }
+        public DateTime Date { get; set; }
+        public int StudentNumber { get; set; }
+        public float Price { get; set; }
+        public int Amount { get; set; }
+    }
+}
