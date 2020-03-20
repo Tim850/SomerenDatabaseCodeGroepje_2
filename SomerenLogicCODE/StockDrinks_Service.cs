bb15294a1e7819a4a1d8cd11302a1f5958ec5991
InvaderCipher <55230649+InvaderCipher@users.noreply.github.com>
2020-03-20 13:07:10 +0100

    Add files via upload

--- SomerenLogicCODE/StockDrinks_Service.cs
+++ SomerenLogicCODE/StockDrinks_Service.cs
@@ -25,12 +25,14 @@ namespace SomerenLogic
                 // something went wrong connecting to the database, so we will add a fake student to the list to make sure the rest of the application continues working!
                 List<StockDrinks> stock = new List<StockDrinks>();
                 StockDrinks a = new StockDrinks();
+                a.DrinkID = 1;
                 a.Name = "testDrank1";
                 a.Price = 2;
                 a.Stock = 13;
                 stock.Add(a);
 
                 StockDrinks b = new StockDrinks();
+                b.DrinkID = 2;
                 b.Name = "testDrank2";
                 b.Price = 1;
                 b.Stock = 7;
@@ -40,6 +42,18 @@ namespace SomerenLogic
                 //throw new Exception("Someren couldn't connect to the database");
             }
 
+        }
+
+        public void UpdateDrinks(string query)
+        {
+            try
+            {
+                stock_db.Db_Update_Drinks(query);
+            }
+            catch (Exception)
+            {
+                //
+            }
         }
     }
 }
