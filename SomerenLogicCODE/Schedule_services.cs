e40a0552ce411e8a428ca73285f51a3aaef13322
IlonkaWagenaar <55359925+IlonkaWagenaar@users.noreply.github.com>
2020-03-27 15:46:19 +0100

    Add files via upload

---
+++ SomerenLogicCODE/Schedule_services.cs
@@ -0,0 +1,41 @@
+﻿using SomerenDAL;
+using SomerenModel;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomerenLogic
+{
+    public class Schedule_Service
+    {
+        Schedule_DAO schedule_db = new Schedule_DAO();
+
+        public List<Schedule> GetSchedule()
+        {
+            //try
+            //{
+                List<Schedule> schedule = schedule_db.Db_Get_All_Schedule();
+                return schedule;
+            //}
+            //catch (Exception)
+            //{
+            //    // something went wrong connecting to the database, so we will add a fake schedule to the list to make sure the rest of the application continues working!
+            //    List<Schedule> schedule = new List<Schedule>();
+            //    Schedule a = new Schedule();
+            //    a.monday = "test";
+            //    a.tuesday = "testt";
+            //    schedule.Add(a);
+            //    Schedule b = new Schedule();
+            //    b.monday = "testing";
+            //    b.tuesday = "testting";
+            //    schedule.Add(b);
+            //    return schedule;
+            //    //throw new Exception("Someren couldn't connect to the database");
+            //}
+
+        }
+    }
+}
